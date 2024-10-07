6779c68ed67b0fc148f87a91454dcf650934c8f0
Davi <davirobergemachado@gmail.com>
2024-10-07 17:31:42 -0300

    comentarios

--- deck de cartas/Deck.py
+++ deck de cartas/Deck.py
@@ -1,38 +1,60 @@
+# Importação da biblioteca datetime para auxiliar na geração de um número pseudo-aleatrório
 from datetime import datetime
 
+# Criação da classe Deck
 class Deck():
   def __init__(self):
+    # Criação de um deck padrão com 52 cartas
     self.numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     self.naipes = ["♥", "♦", "♣", "♠"]
     self.deck = []
-    self.retiradas = []
 
     for i in self.numeros:
       for j in self.naipes:
         self.carta = i+j
         self.deck.append(self.carta)
 
+    # Criação de lista para informar ao usuário quais cartas foram retiradas do baralho
+    self.retiradas = []
 
+
+  # Método para embaralhar o deck
   def emabaralhar(self):
+    # Variável que armazena o último índice da lista deck
     self.ultimo_index = len(self.deck) - 1
+
+    # Equanto o último índice for maior que 0
     while self.ultimo_index > 0:
+      # Gera um número aleatório com base nos microsegundos do instante atual fazendo a operação mod com o último index (assim gerando um número entre 0 e último_index-1)
       self.aleatorio = datetime.now()
       self.index_aleatorio = self.aleatorio.microsecond % self.ultimo_index
+      # Armazena a carta correspondente ao último index da lista
       temp = self.deck[self.ultimo_index]
+      # Troca a última carta pela carta com o index aleatório
       self.deck[self.ultimo_index] = self.deck[self.index_aleatorio]
+      # E troca a carta com o index aleatório pela última carta
       self.deck[self.index_aleatorio] = temp
+      # Decrementa o último index, já que agora a carta que realmente tem o último index já foi embaralhada
       self.ultimo_index -= 1
 
-  def pegar_cartas(self, qtd):
-    for _ in range(qtd):
-      self.retiradas.append(self.deck[0])
-      self.deck.pop(0)
 
+  # Método para retirar cartas, que tem como parâmetro a quantidade de cartas que serão retiradas
+  def pegar_cartas(self, qtd):
+
+    # Por uma quantidade de vezes igual a quantidade de cartas retiradas
+    for _ in range(qtd):
+      # A carta com index 0 (topo do baralho) é adicionada na lista de retiradas
+      self.retiradas.append(self.deck[0])
+      # E é retirada da lista deck
+      self.deck.pop(0)
+    # Retorna a lista de retiradas
     return self.retiradas
   
+  # Função que apenas retorna as cartas retiradas
   def get_retiradas(self):
     return self.retiradas
 
+  # Função que retona a lista deck
   def get_deck(self):
     return self.deck
   
--- deck de cartas/main.py
+++ deck de cartas/main.py
@@ -1,16 +1,23 @@
+# Importa a classe Deck do arquivo Deck
 from Deck import Deck
 
+# Cria objeto deck
 deck = Deck()
 
 while True:
+  # Menu de operações
   print("\n=== DECK DE CARTAS ===\nEscolha o que deseja fazer\n1 - Mostrar o deck\n2 - Retirar cartas do deck\n3 - Embaralhar o deck\n0 - Encerrar o programa\n")
   op = input()
 
   match op:
+
+    # Mostrar o deck
     case "1":
+      # Caso o deck não tenha cartas, informa ao usuário
       if (len(deck.get_deck()) == 0):
         print("\nO deck não tem mais nenhuma carta.")
 
+      # Caso haja, printa as cartas do deck
       else:
         print()
         for i in deck.get_deck():
@@ -19,16 +26,22 @@ while True:
           else:
             print(i, end=", ")
 
+    # Retirar cartas
     case "2":
+      # Caso o deck não tenha cartas para serem retiradas, informa ao usuário
       if (len(deck.get_deck()) == 0):
         print("\nNão há mais cartas para serem retiradas.")
+
+      # Caso haja cartas
       else:
         while True:
           try:
             while True:
+              # O usuário informa a quantidade de cartas que ele deseja retirar
               qtd = int(input(f"\nDigite quantas cartas deseja retirar (quantidade atual: {len(deck.get_deck())}): "))
               if (1 <= qtd <= len(deck.get_deck())):
 
+                # Retira e printa as cartas que foram retiradas
                 for j in deck.pegar_cartas(qtd):
                   if (deck.get_retiradas().index(j) == 0):
                     print(f"\nCartas retiradas: {j}", end=", ")
@@ -48,10 +61,13 @@ while True:
           except ValueError:
             print("\nQuantidade inválida, tente novamente.")
 
+    # Emabaralhar cartas
     case "3":
+      # Chama a função de embaralhar e informa ao usuário
       deck.emabaralhar()
       print("\nDeck embaralhado.")
 
+    # Encerra o programa
     case "0":
       print("\nEncerrando programa...")
       break
